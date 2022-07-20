f7ac689a02c9cabcc6982de4908d1cb0af489363
Philipp Bucher <philipp.bucher@tum.de>
2022-07-20 10:36:34 +0200

    Update updateUbuntu.sh

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -1,12 +1,13 @@
-sudo apt-get dist-upgrade
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-get dist-upgrade
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-get dist-upgrade
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
+#!/bin/bash
+
+# small utility script for updating ubuntu
+# Author: Philipp Bucher
+
+for _ in {1..3} # running multiple times, as sometimes package changes do not get detected in the first run
+do
+    sudo apt-get dist-upgrade
+    sudo apt-get update
+    sudo apt-get autoremove
+    sudo apt-get autoclean
+    echo
+done
